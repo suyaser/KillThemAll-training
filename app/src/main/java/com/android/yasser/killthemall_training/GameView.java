cc782a78ebaa0c702438e938fe3a9d95eb73b3fb
yasser <suyaser0@gmail.com>
2015-01-13 08:16:27 +0200

    change GameView inheritence from View to SurfaceView

--- app/src/main/java/com/android/yasser/killthemall_training/GameView.java
+++ app/src/main/java/com/android/yasser/killthemall_training/GameView.java
@@ -1,21 +1,44 @@
 package com.android.yasser.killthemall_training;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.view.View;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 /**
  * Created by yasser on 1/13/2015.
  */
-public class GameView extends View {
+public class GameView extends SurfaceView {
     private final Bitmap bmp;
+    private final SurfaceHolder holder;
 
     public GameView(Context context)
     {
         super(context);
+        holder = getHolder();
+        holder.addCallback(new SurfaceHolder.Callback(){
+            @SuppressLint("WrongCall")
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                Canvas c = holder.lockCanvas();
+                onDraw(c);
+                holder.unlockCanvasAndPost(c);
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
         bmp = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
     }
 
